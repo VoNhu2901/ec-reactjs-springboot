7a50f732e803e5f3e43e580cbfb6b4808c776656
vonhu <vonhu.alien@gmail.com>
2022-07-04 16:24:45 +0700

    complete config javamailsender

--- backend/assignment/src/main/java/com/example/assignment/config/JavaMailSenderConfiguration.java
+++ backend/assignment/src/main/java/com/example/assignment/config/JavaMailSenderConfiguration.java
@@ -1,20 +1,21 @@
-package com.example.assignment.config;
+// package com.example.assignment.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-@Configuration
-public class JavaMailSenderConfiguration {
-  @Bean
-  public JavaMailSender javaMailSender() {
-    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setProtocol("SMTP");
-    mailSender.setPort(587);
-    mailSender.setUsername("vonhu");
-    mailSender.setPassword("123456");
-    return mailSender;
-  }
-}
+// @Configuration
+// public class JavaMailSenderConfiguration {
+//   @Bean
+//   public JavaMailSender javaMailSender() {
+//     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//     mailSender.setHost("smtp.gmail.com");
+//     mailSender.setProtocol("smtp");
+//     mailSender.setPort(587);
+//     mailSender.setUsername("vonhu.alien@gmail.com");
+//     mailSender.setPassword("rfubvarzbghnlpab");
+//     // mailSender.set
+//     return mailSender;
+//   }
+// }
--- backend/assignment/src/main/java/com/example/assignment/services/impl/EmailServiceImpl.java
+++ backend/assignment/src/main/java/com/example/assignment/services/impl/EmailServiceImpl.java
@@ -3,12 +3,9 @@ package com.example.assignment.services.impl;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -26,6 +23,7 @@ public class EmailServiceImpl implements EmailSender {
   private final JavaMailSender javaMailSender;
 
   @Override
+  @Async
   public void send(String to, String email) {
     try {
       MimeMessage mimeMessage = javaMailSender.createMimeMessage();
@@ -33,7 +31,7 @@ public class EmailServiceImpl implements EmailSender {
       helper.setText(email, true);
       helper.setTo(to);
       helper.setSubject("Confirm your email");
-      helper.setFrom("quocminhh47@gmail.com");
+      helper.setFrom("vonhu@gmail.com");
       javaMailSender.send(mimeMessage);
     } catch (MessagingException e) {
       LOGGER.error("failed to send email", e);
