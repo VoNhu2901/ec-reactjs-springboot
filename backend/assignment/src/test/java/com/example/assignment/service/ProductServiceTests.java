6fba045fc01e98aae64d9cb1fd1e1eeaa69ee037
vonhu <vonhu.alien@gmail.com>
2022-07-11 15:48:22 +0700

    update service/ProductServiceTests.java

--- backend/assignment/src/test/java/com/example/assignment/service/ProductServiceTests.java
+++ backend/assignment/src/test/java/com/example/assignment/service/ProductServiceTests.java
@@ -4,64 +4,72 @@ import com.example.assignment.data.entities.Product;
 import com.example.assignment.data.repositories.CategoryRepository;
 import com.example.assignment.data.repositories.ProductImageRepository;
 import com.example.assignment.data.repositories.ProductRepository;
-import com.example.assignment.dto.request.ProductCreateDto;
 import com.example.assignment.dto.response.ProductResponseDto;
 import com.example.assignment.services.impl.ProductServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class ProductServiceTests {
+    @Mock
     private ProductRepository productRepository;
-    private ModelMapper modelMapper;
-    private ProductServiceImpl productService;
+    @Mock private ModelMapper modelMapper;
 
-    private CategoryRepository categoryRepository;
-    private ProductImageRepository productImageRepository;
+    @Mock private CategoryRepository categoryRepository;
+    @Mock private ProductImageRepository productImageRepository;
 
-    Optional<Product>  productOptional;
+    @InjectMocks private ProductServiceImpl productService;
 
     @BeforeEach
     void setUp() {
-        Product product=new Product();
-        product.setProId(1);
-        productOptional=Optional.of(product);
-        productRepository = mock(ProductRepository.class);
-        categoryRepository = mock(CategoryRepository.class);
-        productImageRepository = mock(ProductImageRepository.class);
-        modelMapper = mock(ModelMapper.class);
-        productService = new ProductServiceImpl(productRepository, modelMapper, categoryRepository, productImageRepository);
+
     }
 
     @Test
-    public void findProductById_WhenRequestValid_ExpectReturnProduct(){
-        ProductResponseDto productDto=mock(ProductResponseDto.class);
-        when(productRepository.findById(anyInt())).thenReturn(productOptional);
-        when(modelMapper.map(productOptional.get(),ProductResponseDto.class)).thenReturn(productDto);
-        assertEquals(productDto,productService.getProductById(anyInt()));
+    void findProductById_WhenRequestValid_ExpectReturnProduct(){
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setProId(1);
+
+        Product product = new Product();
+        when(productRepository.findById(1)).thenReturn(Optional.of(product));
+        when(modelMapper.map(product,ProductResponseDto.class)).thenReturn(productResponseDto);
+
+        ProductResponseDto result = productService.getProductById(1);
+
+        assertThat(result,is(productResponseDto));
+        assertEquals(productResponseDto,result);
     }
 
-    @Test
-    public void updateProduct_WhenRequestValid_ExpectReturnProductAfterUpdate(){
-        ProductCreateDto productCreateDto=mock(ProductCreateDto.class);
-        modelMapper.map(productCreateDto,productOptional.get());
-//        productOptional.get().setUpdateDate(new Date());
-        Product product=mock(Product.class);
-        when(productRepository.save(productOptional.get())).thenReturn(product);
-        when(productRepository.findById(anyInt())).thenReturn(productOptional);
+//    @Test
+//    public void updateProduct_WhenRequestValid_ExpectReturnProductAfterUpdate(){
+//        ProductCreateDto productCreateDto= new ProductCreateDto();
+//
+//        ProductResponseDto productResponseDto = new ProductResponseDto();
+//        Product product = new Product();
+//        when(modelMapper.map(product,ProductResponseDto.class)).thenReturn(productResponseDto);
+//
+//        product.setUpdateDate(new Date());
+//        when(productRepository.save(product)).thenReturn(product);
+//        when(productRepository.findById(anyInt())).thenReturn(Optional.of(product));
+//
+//
 //        assertEquals(modelMapper.map(product,ProductResponseDto.class),productService.updateProduct(anyInt(),productCreateDto));
-    }
+//    }
+
+
 
 }
